eb7578df04d6a984f3b9a79949b4bb8b337c98a0
aiyalcin <ai.yalcin@student.avans.nl>
2025-03-02 23:38:33 +0100

    ingame selectie menu
    --stable--
    Panel toegevoegd met mogelijke te plaatsen sprites.
    
    Verder camera door code laten bewegen

--- WebApi/Controllers/TilesController.cs
+++ WebApi/Controllers/TilesController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.DataBase;
+using WebApi.Items;
 
 namespace WebApi.Controllers
 {
+    [ApiController]
+    //[Authorize]
+    [Route("/tiles")]
     public class TilesController : Controller
     {
-        public IActionResult Index()
+        private readonly string _connectionString;
+        private readonly EnvironmentRepo _environmentRepo;
+        private readonly TilesRepo _tilesRepo;
+        private readonly ILogger<EnvironmentRepo> _logger;
+        public TilesController(IConfiguration configuration, ILogger<EnvironmentRepo> logger)
         {
-            return View();
+            _connectionString = configuration.GetConnectionString("ConnectionString1")
+                                ?? throw new InvalidOperationException("Connection string 'ConnectionString' not found.");
+            _environmentRepo = new EnvironmentRepo(configuration, logger);
+        }
+
+        //get all tiles in an environment
+        [HttpGet("{EnvironmentId}")]
+        public async Task<List<Tile2D?>> ReadTilesAsync(string EnvironmentId)
+        {
+            return await _tilesRepo.ReadTilesAsync(EnvironmentId);
         }
     }
 }
